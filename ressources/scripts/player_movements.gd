c2677cb139fc13a89b16c0a33f56d27a98c66b8e
Supertolek <supertolekmc@gmail.com>
2024-11-24 12:23:05 +0100

    Several small fixes
    - Removed the canvas for the shader on the camera
    - The collision shapes of L-shaped tiles (bottom of wall) to remove the slope
    - Removed the slight tilt on the tilemap which caused visual glitches

--- ressources/scripts/player_movements.gd
+++ ressources/scripts/player_movements.gd
@@ -3,24 +3,7 @@ extends CharacterBody2D
 
 @export var speed = 300.0
 @export var jump_velocity = -400.0
-var coins = 0
-#var time_of_jump = 0
 
-#func _process(delta: float) -> void:
-	##var delta_time = 0
-	##if not is_on_floor():
-		##delta_time = Time.get_ticks_msec() - time_of_jump
-	##elif time_of_jump != 0:
-		##time_of_jump += delta*2
-		##delta_time = Time.get_ticks_msec() - time_of_jump
-	##$Camera2D.zoom = Vector2(1 + (delta_time**.8)*.002, 1 + (delta_time**.8)*.002)
-	#if is_on_floor() and $Camera2D.zoom > Vector2(1, 1):
-		#$Camera2D.zoom -= Vector2(delta*2, delta*2)
-		#if $Camera2D.zoom < Vector2(1, 1):
-			#$Camera2D.zoom = Vector2(1, 1)
-	#elif not is_on_floor():
-		#$Camera2D.zoom += Vector2(delta, delta)
-		
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -30,7 +13,6 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_pressed("move_up") and is_on_floor():
 		velocity.y = jump_velocity
-		#time_of_jump = Time.get_ticks_msec()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actaions.
@@ -51,11 +33,6 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
-#
-#func _on_coin_colision(body: Node2D) -> void:
-	#coins += 1
-	#$CoinCounter.text = "coins : %s" % coins
-#
-#
+
 func tp_back() -> void:
 	position = Vector2(0, - 1)
