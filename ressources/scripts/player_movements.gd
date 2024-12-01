01ce986ffe58167d0cd4fa200fee38bce8526e04
TyAltern <tyalternative3333@gmail.com>
2024-12-01 19:13:09 +0100

    added wall jump

--- ressources/scripts/player_movements.gd
+++ ressources/scripts/player_movements.gd
@@ -1,5 +1,10 @@
 extends CharacterBody2D
 
+@onready var wall_colliding_ray_cast: RayCast2D = $WallCollidingRayCast
+var direction: Vector2
+
+var is_facing_wall: bool = false
+var faced_wall = 0
 
 @export var speed = 300.0
 @export var jump_velocity = -400.0
@@ -7,9 +12,20 @@ extends CharacterBody2D
 @onready var cayote_time = cayote_time_max
 
 
+
 func _process(delta: float) -> void:
 	#$Label.text = str(get_meta("checkpoint_coordinates").x) + str(get_meta("checkpoint_coordinates").y)
-	$Label.text = "cayote time " + str(cayote_time)
+	$Label.text =  str(faced_wall)
+	if wall_colliding_ray_cast.is_colliding():
+		print('face')
+		is_facing_wall = true
+		if direction.x == -1:
+			faced_wall = -1
+		else:
+			faced_wall = 1
+	else:
+		is_facing_wall = false
+		faced_wall = 0
 	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -22,25 +38,31 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_pressed("move_down") and Input.is_action_pressed("jump") and is_on_floor():
 		position.y += 1
-	elif (Input.is_action_pressed("jump") and is_on_floor()) or (Input.is_action_pressed("jump") and cayote_time > 0):
+	elif (Input.is_action_pressed("jump") and is_on_floor()) or (Input.is_action_just_pressed("jump") and cayote_time > 0):
 		velocity.y = jump_velocity
 		cayote_time = 0
 	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actaions.
-	var direction
+	direction = Vector2.ZERO
 	if Input.is_action_pressed("move_left"):
-		direction = -1
+		direction = Vector2.LEFT
 	if Input.is_action_pressed("move_right"):
-		if direction == -1:
-			direction = 0
+		if direction == Vector2.LEFT:
+			direction = Vector2.ZERO
 		else:
-			direction = 1
+			direction = Vector2.RIGHT
 		
 	if direction:
-		velocity.x = direction * speed
-		$PlayerSprite.flip_h = direction < 0
+		wall_colliding_ray_cast.target_position = direction * 32
+		wall_colliding_ray_cast.force_raycast_update()
+		
+				
+		
+		velocity.x = direction.x * speed
+		
+		$PlayerSprite.flip_h = direction == Vector2.LEFT
 		if is_on_floor():
 			$PlayerSprite.play("walking")
 			$PlayerSprite.speed_scale = 1
