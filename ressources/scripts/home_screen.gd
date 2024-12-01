6388ae164b7865a4c26e5627dcacca12ff821f2c
Supertolek <supertolekmc@gmail.com>
2024-12-01 17:48:00 +0100

    Added settings screen

--- ressources/scripts/home_screen.gd
+++ ressources/scripts/home_screen.gd
@@ -8,5 +8,20 @@ func _ready() -> void:
 	resize()
 	get_tree().get_root().size_changed.connect(resize)
 
+
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/platformer.tscn")
+
+
+func _on_settings_button_pressed() -> void:
+	$HomeScreenMenu.visible = false
+	$PanelContainer.visible = true
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_back_button_pressed() -> void:
+	$PanelContainer.visible = false
+	$HomeScreenMenu.visible = true
